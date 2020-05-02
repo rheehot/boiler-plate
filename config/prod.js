175c8cc51e7c119ad768fa80d1534101ed5a9d87
Han Seung Rae <31765530+hhhan0315@users.noreply.github.com>
2020-05-02 16:44:15 +0900

    비밀정보 보호

---
+++ config/prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+  // heroku 랑 동일하게 구성
+  mongoURI: process.env.MONGO_URI,
+};
--- index.js
+++ index.js
@@ -1,24 +1,35 @@
 const express = require("express"); // express 가져오기
 const app = express();
 const port = 5000; // port는 임의지정
+const bodyParser = require("body-parser");
+const { user } = require("./models/user");
+const config = require("./config/key");
+
+app.use(bodyParser.urlencoded({ extended: true })); // application/x-www-form-urlencoded 분석해서 가져옴
+app.use(bodyParser.json()); // application/json
 
 const mongoose = require("mongoose");
-
 mongoose
-  .connect(
-    "mongodb+srv://han:1234@boilerplate-oo9v2.mongodb.net/test?retryWrites=true&w=majority",
-    {
-      // 오류 발생 방지
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    // 오류 발생 방지
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log("MongoDB Conncected..."))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello World!~하이")); // root directory에 오면 출력
+
+app.post("/register", (req, res) => {
+  // 회원가입 정보들을 client에서 가져오면 데이터베이스에 넣기
+  const userNew = new user(req.body); // bodyParser를 활용해서 req.body 활용 가능
+  userNew.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  }); // mongodb method
+});
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
